9964304de42f6e825e15a3b8dad275f2cd7636cc
Mateusz Ramotowski <mateusz.ramotowski@profil-software.com>
2022-09-15 13:40:03 +0200

    PL-3: Created form for create new account-layout

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -9,33 +9,49 @@ import { WrongCredentials } from "./WrongCredentials";
 
 export const LoginForm = () => {
   const [isLoginError, setIsLoginError] = useState(false);
+  const [isCreateAccountForm, setIsCreateAccountForm] = useState(false);
   const login = useLogin();
 
-  const emailInputRef = useRef<HTMLInputElement>(null);
+  const usernameInputRef = useRef<HTMLInputElement>(null);
   const passwordInputRef = useRef<HTMLInputElement>(null);
+  const confirmPasswordInputRef = useRef<HTMLInputElement>(null);
+  const emailInputRef = useRef<HTMLInputElement>(null);
+
+  const toggleShowCreateFormHandler = () => {
+    setIsCreateAccountForm((prev) => !prev);
+  };
 
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
 
-    const enteredEmail = emailInputRef?.current?.value as string;
     const enteredPassword = passwordInputRef?.current?.value as string;
+    const enteredConfirmPassword = confirmPasswordInputRef?.current
+      ?.value as string;
+    const enteredUsername = usernameInputRef?.current?.value as string;
+    const enteredEmail = emailInputRef?.current?.value as string;
 
-    fetchDataPost("/accounts/jwt/create", {
-      username: enteredEmail,
-      password: enteredPassword,
-    })
-      .then((data: tokenInterface) => {
-        setIsLoginError(false);
-        login(data);
+    if (!isCreateAccountForm) {
+      fetchDataPost("/accounts/jwt/create", {
+        username: enteredEmail,
+        password: enteredPassword,
       })
-      .catch((err) => {
-        setIsLoginError(true);
-      });
+        .then((data: tokenInterface) => {
+          setIsLoginError(false);
+          login(data);
+        })
+        .catch((err) => {
+          setIsLoginError(true);
+        });
+    } else {
+      console.log("Request for create account");
+    }
   };
+
+  const loginOrCreate = isCreateAccountForm ? "Create account" : "Login";
 
   return (
     <section className={classes.auth}>
-      <h1>Login</h1>
+      <h1>{loginOrCreate}</h1>
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label>
@@ -44,24 +60,61 @@ export const LoginForm = () => {
               data-testid="username"
               type="text"
               required
-              ref={emailInputRef}
+              ref={usernameInputRef}
             />
           </label>
         </div>
         <div className={classes.control}>
-          <label htmlFor="password">Your Password</label>
-          <input
-            data-testid="password"
-            type="password"
-            id="password"
-            required
-            ref={passwordInputRef}
-          />
+          <label>
+            Your Password
+            <input
+              data-testid="password"
+              type="password"
+              required
+              ref={passwordInputRef}
+            />
+          </label>
         </div>
+        {isCreateAccountForm && (
+          <div className={classes.control}>
+            <label>
+              Confirm Password
+              <input
+                data-testid="confirm-password"
+                type="password"
+                required
+                ref={confirmPasswordInputRef}
+              />
+            </label>
+          </div>
+        )}
+        {isCreateAccountForm && (
+          <div className={classes.control}>
+            <label>
+              Your email
+              <input
+                data-testid="email"
+                type="email"
+                required
+                ref={emailInputRef}
+              />
+            </label>
+          </div>
+        )}
         {isLoginError && <WrongCredentials />}
         <div className={classes.actions}>
-          <button type="submit">Login</button>
+          <button type="submit">{loginOrCreate}</button>
         </div>
+        {!isCreateAccountForm && (
+          <p className={classes.pointner} onClick={toggleShowCreateFormHandler}>
+            Create account
+          </p>
+        )}
+        {isCreateAccountForm && (
+          <p className={classes.pointner} onClick={toggleShowCreateFormHandler}>
+            Login to your account
+          </p>
+        )}
       </form>
     </section>
   );
