f603968d8aaae9b7e36a70e526be1bc37d91fcab
Mateusz Ramotowski <mateusz.ramotowski@profil-software.com>
2022-09-17 17:56:17 +0200

    PL-3: Fix form validation and error handling. Added React-Toastify library

--- src/components/forms/CreateAccountForm.tsx
+++ src/components/forms/CreateAccountForm.tsx
@@ -2,10 +2,14 @@ import React, { useRef, useState } from "react";
 import classes from "./loginForm.module.css";
 import { fetchDataPost } from "../../functions";
 import { Link } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { showMessage, isContainOnlyNumbers } from "../../functions";
 
 export const CreateAccountForm = () => {
-  const [formError, setFormError] = useState("");
-  const [isShowNewAccountMessage, setisShowNewAccountMessage] = useState(false);
+  const [formError, setFormError] = useState({
+    passwordMatch: "",
+    onlyDigits: "",
+  });
   const usernameInputRef = useRef<HTMLInputElement>(null);
   const passwordInputRef = useRef<HTMLInputElement>(null);
   const confirmPasswordInputRef = useRef<HTMLInputElement>(null);
@@ -15,14 +19,41 @@ export const CreateAccountForm = () => {
     const password = passwordInputRef?.current?.value;
     const confirmPassword = confirmPasswordInputRef?.current?.value;
     password !== confirmPassword
-      ? setFormError("Password and confirm password don't match")
-      : setFormError("");
+      ? setFormError((prev) => {
+          return {
+            passwordMatch: "Password and confirm password don't match",
+            onlyDigits: prev.onlyDigits,
+          };
+        })
+      : setFormError((prev) => {
+          return {
+            passwordMatch: "",
+            onlyDigits: prev.onlyDigits,
+          };
+        });
+  };
+
+  const blurHandler = () => {
+    if (isContainOnlyNumbers(passwordInputRef?.current?.value as string)) {
+      setFormError((prev) => {
+        return {
+          passwordMatch: prev.passwordMatch,
+          onlyDigits: "Password can't contain only numbers",
+        };
+      });
+    } else {
+      setFormError((prev) => {
+        return {
+          passwordMatch: prev.passwordMatch,
+          onlyDigits: "",
+        };
+      });
+    }
   };
 
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
     const password = passwordInputRef?.current?.value;
-    const confirmPassword = confirmPasswordInputRef?.current?.value;
     const username = usernameInputRef?.current?.value;
     const email = emailInputRef?.current?.value;
     const path = "/accounts/users/";
@@ -31,17 +62,21 @@ export const CreateAccountForm = () => {
       password: password,
       email: email,
     };
-    if (password === confirmPassword) {
+    if (formError.passwordMatch === "" && formError.onlyDigits === "") {
       fetchDataPost(path, body)
         .then(() => {
-          setisShowNewAccountMessage(true);
-          const timerId = setTimeout(() => {
-            setisShowNewAccountMessage(false);
-            clearTimeout(timerId);
-          }, 5000);
+          showMessage("New account was created!", "info");
         })
         .catch((err) => {
-          setisShowNewAccountMessage(false);
+          for (const property in err.res) {
+            for (const problem of err.res[property]) {
+              if (problem !== undefined) {
+                showMessage(`User not created: ${problem}`, "error");
+              } else {
+                showMessage(`User not created: ${err.errorMessage}`, "error");
+              }
+            }
+          }
         });
     }
   };
@@ -66,6 +101,7 @@ export const CreateAccountForm = () => {
             Your Password
             <input
               onChange={passwordChangeHandler}
+              onBlur={blurHandler}
               data-testid="password"
               type="password"
               required
@@ -96,16 +132,19 @@ export const CreateAccountForm = () => {
             />
           </label>
         </div>
-        {formError !== "" && <p className={classes.alert}>{formError}</p>}
+        {formError.onlyDigits !== "" && (
+          <p className={classes.alert}>{formError.onlyDigits}</p>
+        )}
+        {formError.passwordMatch !== "" && (
+          <p className={classes.alert}>{formError.passwordMatch}</p>
+        )}
         <div className={classes.actions}>
           <button type="submit">Create account</button>
         </div>
         <Link to="/" className={classes.pointner}>
           Login to Your account
         </Link>
-        {isShowNewAccountMessage && (
-          <p className={classes.newUser}>New account was created!</p>
-        )}
+        <ToastContainer />
       </form>
     </section>
   );
--- src/functions.tsx
+++ src/functions.tsx
@@ -1,4 +1,21 @@
-import { credentialsInterface } from "./interafces";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const showMessage = (message: string, messageType: "info" | "error") => {
+  toast[messageType](message, {
+    position: "bottom-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+};
+
+export function isContainOnlyNumbers(str: string) {
+  return /^[0-9]+$/.test(str);
+}
 
 export function fetchDataPost<T>(path: string, body: T): Promise<any> {
   const url = `${process.env.REACT_APP_DOMAIN as string}${path}`;
@@ -14,12 +31,12 @@ export function fetchDataPost<T>(path: string, body: T): Promise<any> {
       return data;
     } else {
       return data.then((data) => {
-        let errorMessage = "Authentication failed!";
+        let errorMessage = "Wrong form data!";
         if (data?.error?.message) {
           errorMessage = data.error.message;
         }
 
-        throw new Error(errorMessage);
+        throw { errorMessage: errorMessage, res: data };
       });
     }
   });
