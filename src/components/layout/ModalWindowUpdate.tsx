35ea3aa61d843d9793547f06d1da74c735b3b774
Mateusz Ramotowski <mateusz.ramotowski@profil-software.com>
2022-10-03 11:37:13 +0200

    Form with pure React using useState

--- src/components/layout/ModalWindowUpdate.tsx
+++ src/components/layout/ModalWindowUpdate.tsx
@@ -2,7 +2,7 @@ import Modal from "react-modal";
 import { ToastContainer } from "react-toastify";
 import { useAppDispatch } from "../../store/hooks";
 import classes from "../../assets/FormCard.module.css";
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {
   id: string;
@@ -22,11 +22,25 @@ const customStyles = {
 };
 
 export const ModalWindowUpdate = (props: Props) => {
+  const [name, setName] = useState("");
+  const [species, setSpecies] = useState("");
+  const [wateringInterval, setWateringInterval] = useState("");
+  const [sunExposure, setSunExposure] = useState("");
+  const [temperature, setTemperature] = useState("");
   const dispatch = useAppDispatch();
+
+  const nameInputHandler = (event: React.ChangeEvent) => setName((event.target as HTMLInputElement).value);
+  const speciesInputHandler = (event: React.ChangeEvent) => setSpecies((event.target as HTMLInputElement).value);
+  const wateringIntervalInputHandler = (event: React.ChangeEvent) =>
+    setWateringInterval((event.target as HTMLInputElement).value);
+  const sunExposureInputHandler = (event: React.ChangeEvent) =>
+    setSunExposure((event.target as HTMLInputElement).value);
+  const temperatureInputHandler = (event: React.ChangeEvent) =>
+    setTemperature((event.target as HTMLInputElement).value);
 
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
-    console.log("Hello submit handler");
+    console.log(name, species, wateringInterval, sunExposure, temperature);
   };
 
   return (
@@ -42,30 +56,62 @@ export const ModalWindowUpdate = (props: Props) => {
           <div className={classes.control}>
             <label>
               Name
-              <input data-testid="name" type="text" required maxLength={50} />
+              <input data-testid="name" type="text" required maxLength={50} value={name} onChange={nameInputHandler} />
             </label>
           </div>
           <div className={classes.control}>
             <label>
               Species
-              <input data-testid="species" type="text" required maxLength={50} />
+              <input
+                data-testid="species"
+                type="text"
+                required
+                maxLength={50}
+                value={species}
+                onChange={speciesInputHandler}
+              />
             </label>
           </div>
           <div className={classes.control}>
             <label>
-              Watering interval <input data-testid="wateringInterval" type="number" required min={1} max={2147483647} />
+              Watering interval{" "}
+              <input
+                data-testid="wateringInterval"
+                type="number"
+                required
+                min={1}
+                max={2147483647}
+                value={wateringInterval}
+                onChange={wateringIntervalInputHandler}
+              />
             </label>
           </div>
           <div className={classes.control}>
             <label>
               Sun exposure
-              <input data-testid="sunExposure" type="number" required min={1} max={24} />
+              <input
+                data-testid="sunExposure"
+                type="number"
+                required
+                min={1}
+                max={24}
+                value={sunExposure}
+                onChange={sunExposureInputHandler}
+              />
             </label>
           </div>
           <div className={classes.control}>
             <label>
               Temperature
-              <input data-testid="temperature" type="number" required min={-100} max={100} />
+              <input
+                data-testid="temperature"
+                type="number"
+                required
+                min={-100}
+                max={100}
+                value={temperature}
+                onChange={temperatureInputHandler}
+              />
             </label>
           </div>
 
