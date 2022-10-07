411dcb36ab3d7439af462908fd880690b1926ce9
Mateusz Ramotowski <mateusz.ramotowski@profil-software.com>
2022-10-07 11:14:00 +0200

    PL-11: Delete useless component inside Autocomplete component

--- src/pages/plants/components/Autocomplete.tsx
+++ src/pages/plants/components/Autocomplete.tsx
@@ -15,67 +15,64 @@ export const Autocomplete = React.forwardRef<HTMLInputElement, Props>((props, fo
     };
   }
 
-  function ComboBox() {
-    function makeSpeciesRequest(search: string) {
-      const config = [["search", search]];
-      getSpiecies(config).then((paginatedList) => {
-        const species = paginatedList.results.map((species) => species.name);
-        setItems(species);
-      });
-    }
-
-    const onSpeciesInputChangeHandler = (inputValue: string | undefined) => {
-      clearTimeout(timerId);
-      if (inputValue) {
-        timerId = setTimeout(makeSpeciesRequest.bind(null, inputValue), 400);
-      }
-    };
-
-    const [items, setItems] = useState<string[]>([]);
-    const {
-      isOpen,
-      getToggleButtonProps,
-      getLabelProps,
-      getMenuProps,
-      getInputProps,
-      getComboboxProps,
-      highlightedIndex,
-      getItemProps,
-      selectedItem,
-    } = useCombobox({
-      onInputValueChange({ inputValue }) {
-        onSpeciesInputChangeHandler(inputValue);
-        setItems(items.filter(getSpeciesFilter(inputValue as string)));
-      },
-      items,
+  function makeSpeciesRequest(search: string) {
+    const config = [["search", search]];
+    getSpiecies(config).then((paginatedList) => {
+      const species = paginatedList.results.map((species) => species.name);
+      setItems(species);
     });
-
-    return (
-      <div>
-        <div className={`w-72 flex flex-col gap-1 ${addedClasses.div}`}>
-          <label className="w-fit" {...getLabelProps()}>
-            Species:
-          </label>
-          <div className="flex shadow-sm bg-white gap-0.5" {...getComboboxProps()}>
-            <input placeholder="choose species" className="w-full p-1.5" {...getInputProps()} ref={forwardedRef} />
-            <button aria-label="toggle menu" className="px-2" type="button" {...getToggleButtonProps()}>
-              {isOpen ? <>&#8593;</> : <>&#8595;</>}
-            </button>
-          </div>
-        </div>
-        <ul
-          {...getMenuProps()}
-          className={`absolute p-0 w-72 bg-white shadow-md max-h-80 overflow-scroll ${addedClasses.ul}`}
-        >
-          {isOpen &&
-            items.map((item, index) => (
-              <li key={`${item}${index}`} {...getItemProps({ item, index })}>
-                <span>{item}</span>
-              </li>
-            ))}
-        </ul>
-      </div>
-    );
   }
-  return <ComboBox />;
+
+  const onSpeciesInputChangeHandler = (inputValue: string | undefined) => {
+    clearTimeout(timerId);
+    if (inputValue) {
+      timerId = setTimeout(makeSpeciesRequest.bind(null, inputValue), 400);
+    }
+  };
+
+  const [items, setItems] = useState<string[]>([]);
+  const {
+    isOpen,
+    getToggleButtonProps,
+    getLabelProps,
+    getMenuProps,
+    getInputProps,
+    getComboboxProps,
+    highlightedIndex,
+    getItemProps,
+    selectedItem,
+  } = useCombobox({
+    onInputValueChange({ inputValue }) {
+      onSpeciesInputChangeHandler(inputValue);
+      setItems(items.filter(getSpeciesFilter(inputValue as string)));
+    },
+    items,
+  });
+
+  return (
+    <div>
+      <div className={`w-72 flex flex-col gap-1 ${addedClasses.div}`}>
+        <label className="w-fit" {...getLabelProps()}>
+          Species:
+        </label>
+        <div className="flex shadow-sm bg-white gap-0.5" {...getComboboxProps()}>
+          <input placeholder="choose species" className="w-full p-1.5" {...getInputProps()} ref={forwardedRef} />
+          <button aria-label="toggle menu" className="px-2" type="button" {...getToggleButtonProps()}>
+            {isOpen ? <>&#8593;</> : <>&#8595;</>}
+          </button>
+        </div>
+      </div>
+      <ul
+        {...getMenuProps()}
+        className={`absolute p-0 w-72 bg-white shadow-md max-h-80 overflow-scroll ${addedClasses.ul}`}
+      >
+        {isOpen &&
+          items.map((item, index) => (
+            <li key={`${item}${index}`} {...getItemProps({ item, index })}>
+              <span>{item}</span>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 });
